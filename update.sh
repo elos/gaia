2fc121c094cb721f2bc1521f853e9d1b09c9d7ad
Nick Landolfi <nclandolfi@gmail.com>
2016-08-08 17:21:12 -0700

    tweak update script to work with new jobs model

--- update.sh
+++ update.sh
@@ -10,15 +10,15 @@ else
 	echo "No server running"
 fi
 
-if [ -e gaia ]; then
+if [ -e gaiamain ]; then
 	echo "Removing gaia"
-	rm gaia
+	rm gaiamain
 fi
 
 echo "Downloading gaia"
-wget https://github.com/elos/gaia/blob/master/serve/build/linux/gaia?raw=true -O gaia
-chmod +x gaia
-sudo ./gaia -dbtype=mongo -dbaddr=localhost:27017 -appdir=/home/ubuntu/go/src/github.com/elos/gaia/app -certfile=/etc/letsencrypt/live/elos.pw/fullchain.pem -keyfile=/etc/letsencrypt/live/elos.pw/privkey.pem -port=443 > ~/stdout.txt 2> ~/stderr.txt &
+wget https://github.com/elos/gaia/blob/master/serve/build/linux/gaia?raw=true -O gaiamain
+chmod +x gaiamain
+sudo ./gaiamain -dbtype=mongo -dbaddr=localhost:27017 -appdir=/home/ubuntu/go/src/github.com/elos/gaia/app -certfile=/etc/letsencrypt/live/elos.pw/fullchain.pem -keyfile=/etc/letsencrypt/live/elos.pw/privkey.pem -port=443 > ~/stdout.txt 2> ~/stderr.txt &
 #sudo ./main -dbtype=mongo -dbaddr=localhost:27017 -appdir=/home/ubuntu/go/src/github.com/elos/gaia/app > ~/stdout.txt 2> ~/stderr.txt &
 echo "Writing pid file"
 echo $! > ~/server.pid
