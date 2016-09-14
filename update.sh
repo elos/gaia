fd86bbdd7d9f26cbe40a4df2e1df2e4bf89ef29d
Nick Landolfi <nclandolfi@gmail.com>
2016-09-13 20:56:28 -0700

    update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ if [ -e gaiamain ]; then
 fi
 
 echo "Downloading gaia"
-wget https://github.com/elos/gaia/blob/master/serve/build/linux/gaia?raw=true -O gaiamain
+wget https://github.com/elos/gaia/blob/nlandolfi/form/serve/build/linux/gaia?raw=true -O gaiamain
 chmod +x gaiamain
 sudo ./gaiamain -dbtype=mongo -dbaddr=localhost:27017 -appdir=/home/ubuntu/go/src/github.com/elos/gaia/app -certfile=/etc/letsencrypt/live/elos.pw/fullchain.pem -keyfile=/etc/letsencrypt/live/elos.pw/privkey.pem -port=443 > ~/stdout.txt 2> ~/stderr.txt &
 #sudo ./main -dbtype=mongo -dbaddr=localhost:27017 -appdir=/home/ubuntu/go/src/github.com/elos/gaia/app > ~/stdout.txt 2> ~/stderr.txt &
